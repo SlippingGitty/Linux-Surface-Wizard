9dba4af96fe67e123ee9f0ce06ef4d70745b1893
SlippingGitty <76500838+SlippingGitty@users.noreply.github.com>
2021-05-03 12:26:23 -0500

    sudo ==> su -c

--- SurfaceLinuxSetupWizard.sh
+++ SurfaceLinuxSetupWizard.sh
@@ -6,7 +6,7 @@
 #   |___/ \__/|_|_\_||_||_|\__/___| |___|_|_|\__|\__//_/ \_\ |___/|___| |_|  \__/|_|   !_/ \_!|_|___|_||_|_|_\__/  
 #
 #                                           
-#            VERSION 2021.05.01.01
+#            VERSION 2021.05.03.02
 
 #INITIAL INFO PROMPT
 dialog  --title "Linux-Surface Setup Wizard" --infobox "Welcome. \n\nIf this is your first time running this script, please select option 1 before attempting to do anything else. \n\nAfterwards, select a function by typing the number that co-relates with the specific action you want to do." 10 70;sleep 5 
@@ -40,32 +40,31 @@ OPTIONS=(1  "Import keys and add repo"
 clear
     case $CHOICE in
             1) 
-                wget -qO - https://raw.githubusercontent.com/linux-surface/linux-surface/master/pkg/keys/surface.asc \
-    | gpg --dearmor | sudo dd of=/etc/apt/trusted.gpg.d/linux-surface.gpg && echo "deb [arch=amd64] https://pkg.surfacelinux.com/debian release main" \
-	| sudo tee /etc/apt/sources.list.d/linux-surface.list && sudo apt update
+               su -c "wget -qO - https://raw.githubusercontent.com/linux-surface/linux-surface/master/pkg/keys/surface.asc \
+    | gpg --dearmor | dd of=/etc/apt/trusted.gpg.d/linux-surface.gpg && echo 'deb [arch=amd64] https://pkg.surfacelinux.com/debian release main' \
+	| tee /etc/apt/sources.list.d/linux-surface.list && apt update"
             # Signs the repos needed to install the surface-linux packages
                 ;;
             2)
-                sudo apt install linux-image-surface linux-headers-surface
+                su -c "apt install linux-image-surface linux-headers-surface"
             # Installs the surface-linux modules and headers
                 ;;
             3)
-                sudo apt install iptsd libwacom-surface && sudo systemctl enable iptsd
+                su -c "apt install iptsd libwacom-surface && sudo systemctl enable iptsd"
             # Installs and enables touchscreen support
                 ;;
             4)
-                sudo apt install linux-surface-secureboot-mok
+                su -c "apt install linux-surface-secureboot-mok"
             # Installs and enables secure boot
                 ;;
             5)
                 dialog  --title "Surface-Linux Setup Wizard" --infobox "This is going to remove GRUB_TIME=0 from your /etc/default/grub file. \n\nGRUB is a bootloader, and that's like, very important! \n\nYou can manually add this back yourself if you wish to hide it again." 10 70;sleep 5
-                sudo cp /etc/default/grub /etc/default/grub.bak
-		        sudo sed -i 's/\<GRUB_TIMEOUT=0\>//g' /etc/default/grub
-                sudo update-grub
+                clear
+                su -c "cp /etc/default/grub /etc/default/grub.bak && sed -i 's/\<GRUB_TIMEOUT=0\>//g' /etc/default/grub && update-grub"
              # Removes the timeout for GRUB and shows the menu
                 ;;
             6)
-                sudo update-grub
+                su -c "update-grub"
                 exit
                 ;;
     esac
