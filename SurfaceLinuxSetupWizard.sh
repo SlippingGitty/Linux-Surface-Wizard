49a35d12e8dc9107cc58193d21df08f9d5cdf147
Vishram1123 <64966832+Vishram1123@users.noreply.github.com>
2021-05-08 11:43:28 -0400

    Update SurfaceLinuxSetupWizard.sh
    Check if linux-surface already exists and if dialog exists

--- SurfaceLinuxSetupWizard.sh
+++ SurfaceLinuxSetupWizard.sh
@@ -9,8 +9,14 @@
 #            VERSION 2021.05.03.02
 
 #INITIAL INFO PROMPT
-dialog  --title "Linux-Surface Setup Wizard" --infobox "Welcome. \n\nIf this is your first time running this script, please select option 1 before attempting to do anything else. \n\nAfterwards, select a function by typing the number that co-relates with the specific action you want to do." 10 70;sleep 5 
-
+if dialog  --title "Linux-Surface Setup Wizard" --infobox "Welcome. \n\nIf this is your first time running this script, please select option 1 before attempting to do anything else. \n\nAfterwards, select a function by typing the number that co-relates with the specific action you want to do." 10 70;
+then
+    sleep 5
+else
+    echo "You may need to install dialog with 'apt install dialog'. Do this, then rerun this script"
+    sleep 2
+    exit
+fi
 #DIALOG/NCURSES
 HEIGHT=15
 WIDTH=40
@@ -40,9 +46,14 @@ OPTIONS=(1  "Import keys and add repo"
 clear
     case $CHOICE in
             1) 
-               su -c "wget -qO - https://raw.githubusercontent.com/linux-surface/linux-surface/master/pkg/keys/surface.asc \
+	    	if ls /etc/apt/sources.list.d | grep -F "linux-surface.list" 
+		then
+		d	ialog  --title "Linux-Surface Setup Wizard" --infobox 'You already have the "linux-surface" repo initialized' 10 70;
+		else
+               		su -c "wget -qO - https://raw.githubusercontent.com/linux-surface/linux-surface/master/pkg/keys/surface.asc \
     | gpg --dearmor | dd of=/etc/apt/trusted.gpg.d/linux-surface.gpg && echo 'deb [arch=amd64] https://pkg.surfacelinux.com/debian release main' \
 	| tee /etc/apt/sources.list.d/linux-surface.list && apt update"
+		fi
             # Signs the repos needed to install the surface-linux packages
                 ;;
             2)
@@ -58,7 +69,7 @@ clear
             # Installs and enables secure boot
                 ;;
             5)
-                dialog  --title "Surface-Linux Setup Wizard" --infobox "This is going to remove GRUB_TIME=0 from your /etc/default/grub file. \n\nGRUB is a bootloader, and that's like, very important! \n\nYou can manually add this back yourself if you wish to hide it again." 10 70;sleep 5
+                dialog  --title "Surface-Linux Setup Wizard" --infobox "This is going to remove GRUB_TIMEOUT=0 from your /etc/default/grub file. \n\nGRUB is a bootloader, and that's like, very important! \n\nYou can manually add this back yourself if you wish to hide it again." 10 70;sleep 5
                 clear
                 su -c "cp /etc/default/grub /etc/default/grub.bak && sed -i 's/\<GRUB_TIMEOUT=0\>//g' /etc/default/grub && update-grub"
              # Removes the timeout for GRUB and shows the menu
