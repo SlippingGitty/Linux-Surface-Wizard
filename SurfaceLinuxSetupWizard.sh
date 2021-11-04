cb8a305788a6afbecee2d93ff41bfa6f1e4cc5a8
SlippingGitty <76500838+SlippingGitty@users.noreply.github.com>
2021-11-04 18:06:44 -0500

    Update SurfaceLinuxSetupWizard.sh

--- SurfaceLinuxSetupWizard.sh
+++ SurfaceLinuxSetupWizard.sh
@@ -1,28 +1,25 @@
 #!/usr/bin/env bash
-# Linux-Surface-Wizard is licensed under MIT license, see https://github.com/SlippingGitty/Linux-Surface-Wizard/blob/main/LICENSE for more info
-#    __  _  _ ___ ___ __   ______   _   _ __  _ _  ___   __   __  ___ _____ _  _ ___   _   _  _ ___  __  ___ __   
-#   /' _/| || | _ \ __/  \ / _/ __| | | | |  \| | || \ \_/ / /' _/| __|_   _| || | _,\ | | | || |_  |/  \| _ \ _\  
-#   `._`.| \/ | v / _| /\ | \_| _|  | |_| | | ' | \/ |> , <  `._`.| _|  | | | \/ | v_/ | 'V' || |/ /| /\ | v / v | 
-#   |___/ \__/|_|_\_||_||_|\__/___| |___|_|_|\__|\__//_/ \_\ |___/|___| |_|  \__/|_|   !_/ \_!|_|___|_||_|_|_\__/  
-#
-#                                           
-#            VERSION 2021.05.03.02
+# Linux-Surface-Wizard is licensed under MIT license, see https://github.com/SlippingGitty/Linux-Surface-Wizard/blob/main/LICENSE for more info                                                                          
+# _|      _|                      _|  _|  _|            _|      _|_|            
+# _|      _|    _|_|_|  _|_|_|        _|  _|    _|_|_|        _|      _|    _|  
+# _|      _|  _|    _|  _|    _|  _|  _|  _|  _|    _|  _|  _|_|_|_|  _|    _|  
+#   _|  _|    _|    _|  _|    _|  _|  _|  _|  _|    _|  _|    _|      _|    _|  
+#     _|        _|_|_|  _|    _|  _|  _|  _|    _|_|_|  _|    _|        _|_|_|  
+#                                                                           _|  
+#                                                                       _|_|                                               
+#            VERSION 2021.11.04.01
 
 #INITIAL INFO PROMPT
-if dialog  --title "Linux-Surface Setup Wizard" --infobox "Welcome. \n\nIf this is your first time running this script, please select option 1 before attempting to do anything else. \n\nAfterwards, select a function by typing the number that co-relates with the specific action you want to do." 10 70;
-then
-    sleep 5
-else
-    echo "You may need to install dialog with 'apt install dialog'. Do this, then rerun this script"
-    sleep 2
-    exit
-fi
+dialog  --title "Vanillaify" --infobox "Welcome. \n\nThis script will help you install vanilla GNOME, remove snapd, and give you other various QOL options.  \n\nIf you would like to help with this project, please consider contributing! Be sure to report any issues on SlippingGitty/Vanillaify" 10 70;sleep 5 
+dialog  --title "⚠ CAREFUL ⚠" --infobox "WARNING. \n\nIf you have no idea what the following options are, chances are you should not be messing with them. \n\nPlease close this ASAP and read the Github page for more information." 10 70;sleep 5 
+
+
 #DIALOG/NCURSES
 HEIGHT=15
 WIDTH=40
 CHOICE_HEIGHT=4
-BACKTITLE="Apt Linux-Surface Setup Wizard"
-TITLE="Installer"
+BACKTITLE="Vanillaify Toolbox 🔧"
+TITLE="▼ Installer ▼"
 MENU="Select an option:"
 
 while [ "$CHOICE -ne 4" ]; do
@@ -35,48 +32,66 @@ while [ "$CHOICE -ne 4" ]; do
                 2>&1 >/dev/tty)
 
 #OPTIONS NAMES
-OPTIONS=(1  "Import keys and add repo"
-         2  "Install kernel modules"
-         3  "Install touchscreen support"
-         4  "Enable secure boot"
-         5  "Show GRUB menu"
-         6  "Update GRUB and Finish")
+OPTIONS=(1  "Add GNOME 40 PPA && Install"
+         2  "Install ALL GNOME packages"
+         3  "Switch to Adwaita theme"
+         4  "Download GNOME 40 wallpapers"
+         5  "Install QoL packages"
+         6  "Remove Ubuntu logo from GDM"
+         7  "Remove all of snapd"
+         8  "Finish ☑")
 
 #OPTIONS IN EXECUTION
 clear
     case $CHOICE in
             1) 
-	    	if ls /etc/apt/sources.list.d | grep -F "linux-surface.list" 
-		then
-		d	ialog  --title "Linux-Surface Setup Wizard" --infobox 'You already have the "linux-surface" repo initialized' 10 70;
-		else
-               		su -c "wget -qO - https://raw.githubusercontent.com/linux-surface/linux-surface/master/pkg/keys/surface.asc \
-    | gpg --dearmor | dd of=/etc/apt/trusted.gpg.d/linux-surface.gpg && echo 'deb [arch=amd64] https://pkg.surfacelinux.com/debian release main' \
-	| tee /etc/apt/sources.list.d/linux-surface.list && apt update"
-		fi
-            # Signs the repos needed to install the surface-linux packages
+                su -c "add-apt-repository ppa:shemgp/gnome-40 && apt-get update && apt-get install mutter=40.0-0shemgpubuntu6 -y && apt-get install gnome-shell=40.0-0shemgpubuntu3 -y && apt-get install gnome-session -y && apt-get install fonts-cantarell && apt-get upgrade -y
+                echo Gnome 40 installed"
+                sleep 5
+            # Adds GNOME 40 repo and installs GNOME 40 and fonts
                 ;;
             2)
-                su -c "apt install linux-image-surface linux-headers-surface surface-ath10k-firmware-override"
-            # Installs the surface-linux modules and headers
+                su -c "apt install gnome"
+                echo the GNOME suite of applications are now installed
+                sleep 5
+            # Install ALL GNOME packages such as games, weather, media players, etc.
                 ;;
             3)
-                su -c "apt install iptsd libwacom-surface && systemctl enable iptsd"
-            # Installs and enables touchscreen support
+                su -c "apt-get install adwaita-icon-theme-full" 
+                gsettings set org.gnome.desktop.interface gtk-theme Adwaita | gsettings set org.gnome.desktop.interface icon-theme Adwaita
+                echo Switched to Adwaita theme 
+                sleep 3
+            # Installs Switches theme to Adwaita
                 ;;
             4)
-                su -c "apt install linux-surface-secureboot-mok"
-            # Installs and enables secure boot
+                cd Pictures/
+                git clone https://gitlab.gnome.org/GNOME/gnome-backgrounds.git
+                cd
+            # Downloads all of the latest GNOME wallpapers into the Pictures directory
                 ;;
             5)
-                dialog  --title "Surface-Linux Setup Wizard" --infobox "This is going to remove GRUB_TIMEOUT=0 from your /etc/default/grub file. \n\nGRUB is a bootloader, and that's like, very important! \n\nYou can manually add this back yourself if you wish to hide it again." 10 70;sleep 5
-                clear
-                su -c "cp /etc/default/grub /etc/default/grub.bak && sed -i 's/\<GRUB_TIMEOUT=0\>//g' /etc/default/grub && update-grub"
-             # Removes the timeout for GRUB and shows the menu
+                su -c "apt-get install chrome-gnome-shell gnome-tweaks gnome-software"
+                echo chrome-gnome-shell, tweaks, and the GNOME Software package manager is now installed
+                sleep 3
+            # Quallity of life GNOME things
                 ;;
             6)
-                su -c "update-grub"
+                cd /usr/share/plymouth/
+                su -c "mv ubuntu-logo.png ubuntu-logo.png.bak"
+                cd
+                echo Ubuntu logo has been removed from GDM
+                sleep 3
+             # Removes the Ubuntu logo from GDM
+                ;;
+            7)
+                su -c "snap remove snap-store | snap remove gtk-common-themes | snap remove gnome-3-34-1804 | snap remove core18 | snap remove snapd | umount /var/snap | apt purge snapd | rm -rf ~/snap | rm -rf /snap && rm -rf /var/snap | rm -rf /var/lib/snapd | apt-mark hold snapd"
+                echo The bulk of snapd is removed
+                sleep 5 
+            # Completely removes snapd and installed snap packages
+                ;;
+            8)
                 exit
+            # Break
                 ;;
     esac
 done
